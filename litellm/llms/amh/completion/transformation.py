d9459e6b9e273f43b59c7ede706e19fad859b785
Yiping Kang <yiping.kang@ally.com>
2026-01-20 22:02:23 -0500

    revert back the debug change

--- litellm/llms/amh/completion/transformation.py
+++ litellm/llms/amh/completion/transformation.py
@@ -188,8 +188,6 @@ class AMHConfig(BaseConfig):
 
         response_str = raw_response.json()["output"]["choices"][0]["message"]["content"]
 
-        response_str = "THIS IS GOING TO BREAK EVEYRTHING"
-
         completion_response = {}
         completion_response['generated_text'] = response_str
 
